748e6358145730256547348f5a03902a4a0d67d9
yuzo <shiroyamayuzo@gmail.com>
2026-02-21 16:27:36 +0900

    Add configuration file for UmVirt LFSAutoBuilder

---
+++ config/config.sh
@@ -0,0 +1,17 @@
+#
+# UmVirt LFSAutoBuilder Configuration file
+# ----------------------------------------
+#
+# Target architecture
+#
+# Possible values: "amd64", "i686", "i586"
+# Default: "amd64"
+#
+LFS_TARGET_ARCH=amd
+#
+# Target init 
+#
+# Possible values: "sysv", "systemd"
+# Default: "sysv"
+#
+LFS_TARGET_INIT=systemd
