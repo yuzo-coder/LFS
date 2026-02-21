d05d2fa44dadd09f616405e7881110216a0512c3
yuzo <shiroyamayuzo@gmail.com>
2026-02-21 16:28:12 +0900

    Set default values for DOWNLOAD_APP and INSECURE_MODE

---
+++ config/downloaderconfig.sh
@@ -0,0 +1,23 @@
+#
+# UmVirt LFSAutoBuilder Downloader Configuration file
+# ---------------------------------------------------
+#
+# Download application
+#
+# Possible values: "wget", "curl"
+# Default: "wget"
+#
+DOWNLOAD_APP=wget
+#
+# Insecure mode
+#
+# Possible values: "YES", "NO"
+# Default: "NO"
+#
+INSECURE_MODE=NO
+#
+# CURL proxy
+#
+# Possible values: [protocol://]host[:port]
+# Default: ""
+CURL_PROXY=
