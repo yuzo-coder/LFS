d4c939ac4162dd6f978a041f2690db0240105a75
yuzo <shiroyamayuzo@gmail.com>
2026-03-06 08:06:47 +0900

    Update gemini0.sh

--- blfs/gemini0.sh
+++ blfs/gemini0.sh
@@ -67,32 +67,26 @@ echo "===== Installing systemd unit ====="
 # /usr/lib/systemd/system/ ディレクトリが存在することを確認
 mkdir -p /usr/lib/systemd/system
 
-cat > /etc/ssh/sshd_config << "EOF"
-Port 22
-PasswordAuthentication yes
-PermitRootLogin yes
+cat > /usr/lib/systemd/system/sshd.service << "EOF"
+[Unit]
+Description=OpenSSH Daemon
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/sbin/sshd -D
+ExecReload=/bin/kill -HUP $MAINPID
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
 EOF
-
-# cat > /usr/lib/systemd/system/sshd.service << "EOF"
-# [Unit]
-# Description=OpenSSH Daemon
-# After=network.target
-
-# [Service]
-# Type=simple
-# ExecStart=/usr/sbin/sshd -D
-# ExecReload=/bin/kill -HUP $MAINPID
-# KillMode=process
-# Restart=on-failure
-
-# [Install]
-# WantedBy=multi-user.target
-# EOF
 
 # 反映と有効化
 systemctl daemon-reload
 # 既に有効化されている場合のエラーを避けるため
-systemctl restart sshd
 systemctl enable sshd || true
 
 echo "===== PHASE0 COMPLETE ====="
+
