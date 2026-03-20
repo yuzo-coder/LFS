95d7a1ce384c009c601c655cfe2f9894869e03c6
yuzo <shiroyamayuzo@gmail.com>
2026-03-20 09:12:05 +0900

    Update 01-pam-sway.sh

--- blfs/01-pam-sway.sh
+++ blfs/01-pam-sway.sh
@@ -110,7 +110,9 @@ build_meson systemd "https://github.com/systemd/systemd/archive/v257.8/systemd-2
 # --- 6. Wayland & Graphics Foundation ---
 build_autotools libxml2 "https://download.gnome.org/sources/libxml2/2.12/libxml2-2.12.7.tar.xz" "--disable-static --without-python"
 build_autotools hwdata "https://github.com/vcrhonek/hwdata/archive/v0.404/hwdata-0.404.tar.gz" ""
-build_cmake json-c "https://s3.amazonaws.com/json-c_releases/releases/json-c-0.18.tar.gz" ""
+
+build_cmake doxygen "https://doxygen.nl/files/doxygen-1.16.1.src.tar.gz" "-DCMAKE_BUILD_TYPE=Release"
+build_cmake json-c "https://s3.amazonaws.com/json-c_releases/releases/json-c-0.18.tar.gz" "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
 
 build_meson wayland "https://gitlab.freedesktop.org/wayland/wayland/-/releases/1.23.0/downloads/wayland-1.23.0.tar.xz" "-Ddocumentation=false"
 build_meson wayland-protocols "https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/1.36/downloads/wayland-protocols-1.36.tar.xz" ""
